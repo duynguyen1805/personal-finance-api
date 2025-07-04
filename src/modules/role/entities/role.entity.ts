247522e1bb0d3831ff42f40b8cbe13660e724512
TDN <duynguyenqwert@gmail.com>
2025-07-04 10:12:41 +0700

    update: role enum

--- src/modules/role/entities/role.entity.ts
+++ src/modules/role/entities/role.entity.ts
@@ -1,13 +1,14 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { BaseEntity } from '../../../common/base/base.entity';
+import { ERole } from '../dto/enum-role.dto';
 
 @Entity()
 export class Role extends BaseEntity {
   @PrimaryGeneratedColumn('increment')
   id: number;
-  
+
   @Column()
-  name: string;
+  name: ERole;
 
   @Column()
   permissions: string;
