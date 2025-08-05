a9aabbe77f8b15d1114b056b5be255826f18db98
TDN <duynguyenqwert@gmail.com>
2025-08-05 09:58:09 +0700

    fix: create user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -98,6 +98,7 @@ export class UserService {
   }): Promise<User> {
     const user = this.userRepository.create({
       ...userData,
+      walletAddress: 'NULL',
       status: 'ACTIVE' as any,
       theme: 'light',
       currency: 'VND'
